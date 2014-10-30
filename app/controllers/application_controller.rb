f57ac91e666ad564a0ae22d46fb15ff4cef8fd67
aprowe <aprowe@ucsc.edu>
2014-10-29 23:43:46 -0700

    Added number messaging system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,30 @@ class ApplicationController < ActionController::Base
     render text: 'fail'
   end
 
+  def sendTexts
+    require "uri"
+    require "net/http"
+
+
+    message = ''
+    Renter.
+      where( {paid: false} ).where.not( phone: false ).each do |renter|
+
+        message = "Dear #{renter.name}, this months rent is $#{renter.rent}. Make a check out to #{House.first.rent}, or paypal to aprowe@ucsc.edu. Thanks!"
+
+        params = {message: message, number: renter.phone}
+        x = Net::HTTP.post_form(URI.parse('http://textbelt.com/text'), params)
+
+        logger.debug x
+        logger.debug "Message sent to #{renter.name}"
+
+        sleep 61
+    end
+
+    render text: 'Done'
+
+  end
+
 
   def login
     user = Renter.find_by_name(params[:username])
